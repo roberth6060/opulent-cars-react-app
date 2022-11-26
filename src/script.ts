8613fb93bbce7353629621646e93095592d12157
Robert C. Haye <roberthaye1994@gmail.com>
2022-11-26 22:17:34 +0100

    migrated leaflet map to typescript

---
+++ src/script.ts
@@ -0,0 +1,25 @@
+/* ========== Displaying a Map Using Leaflet Library ========== */
+let map;
+let mapEvent;
+const latitude = 45.7988904;
+const longitude = 15.8869043;
+map = L.map("map").setView([latitude, longitude], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+//Mark location on map
+L.marker([latitude, longitude])
+  .addTo(map)
+  .bindPopup(
+    L.popup({
+      maxWidth: 150,
+      minWidth: 100,
+      autoClose: false,
+      closeOnClick: false,
+    })
+  )
+  .setPopupContent("Savska opatovina 96, 10070, Zagreb, Croatia") //fake address
+  .openPopup();
