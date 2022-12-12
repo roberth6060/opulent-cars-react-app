1502d0cb1ff2cd48f559fa1ead14048b9fc15511
Robert C. Haye <roberthaye1994@gmail.com>
2022-12-12 12:23:23 +0100

    completed data for suv collection

--- src/collection.ts
+++ src/collection.ts
@@ -14,7 +14,7 @@ const sportsCarCollection: CollectionType[] = [
   {
     carName: "2022 Lamborghini Huracan",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=lamborghini&modelFamily=huracan&modelRange=huracan-evo&modelVariant=co&modelYear=2022&powerTrain=petrol&transmission=0&bodySize=2&trim=eu&paintId=pspc0041&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=lamborghini&modelFamily=huracan&modelRange=huracan-evo&modelVariant=co&modelYear=2022&powerTrain=petrol&transmission=0&bodySize=2&trim=eu&paintId=pspc0029&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
     carLbs: "413",
     carRwhp: "631",
     carPrice: "261,497",
@@ -34,17 +34,17 @@ const sportsCarCollection: CollectionType[] = [
   {
     carName: "2016 McLaren 675LT Spider",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=mclaren&modelFamily=540c&modelRange=540c&modelVariant=co&modelYear=2018&powerTrain=petrol&transmission=0&bodySize=2&trim=eu&paintId=pspc0041&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=mclaren&modelFamily=540c&modelRange=540c&modelVariant=co&modelYear=2018&powerTrain=petrol&transmission=0&bodySize=2&trim=eu&paintId=pspc0146&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
     carLbs: "2,927",
     carRwhp: "666",
     carPrice: "313,000",
     carType: "rwd",
-    url: "",
+    url: "https://www.edmunds.com/mclaren/675lt-spider/2016/vin/SBM11SAA7GW675766/?radius=100",
   },
   {
     carName: "2020 Mercedes-Benz AMG® GT",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=mercedes&modelFamily=amg-gt&modelRange=amg-gt&modelVariant=ca&modelYear=2020&powerTrain=fossil&transmission=0&bodySize=2&trim=0&paintId=pspc0041&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=mercedes&modelFamily=amg-gt&modelRange=amg-gt&modelVariant=ca&modelYear=2018&powerTrain=fossil&transmission=0&bodySize=2&trim=0&paintId=pspc0075&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
     carLbs: "3560",
     carRwhp: "577",
     carPrice: "185,818",
@@ -54,7 +54,7 @@ const sportsCarCollection: CollectionType[] = [
   {
     carName: "2019 Toyota Supra",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=audi&modelFamily=r8&modelRange=r8&modelVariant=ca&modelYear=2021&powerTrain=fossil&transmission=0&bodySize=2&trim=performance&paintId=pspc0014&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=audi&modelFamily=r8&modelRange=r8&modelVariant=ca&modelYear=2021&powerTrain=fossil&transmission=0&bodySize=2&trim=performance&paintId=pspc0039&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
     carLbs: "3516.4",
     carRwhp: "532",
     carPrice: "166,981",
@@ -68,7 +68,7 @@ const muscleCarCollection: CollectionType[] = [
   {
     carName: "2021 Ford Mustang",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=ford&modelFamily=mustang&modelRange=mach-1&modelVariant=co&modelYear=2021&powerTrain=fossil&transmission=0&bodySize=2&trim=0&paintId=pspc0096&angle=01",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=ford&modelFamily=mustang&modelRange=mach-1&modelVariant=co&modelYear=2021&powerTrain=fossil&transmission=0&bodySize=2&trim=0&paintId=pspc0004&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
     carLbs: "365",
     carRwhp: "335",
     carPrice: "49,990",
@@ -76,32 +76,32 @@ const muscleCarCollection: CollectionType[] = [
     url: "https://www.carmax.com/car/beta/23246405",
   },
   {
-    carName: "2019 Toyota Supra",
+    carName: "2022 Dodge Challenger",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=lamborghini&modelFamily=aventador&modelRange=aventador-svj&modelVariant=co&modelYear=2019&powerTrain=petrol&transmission=0&bodySize=2&trim=eu&paintId=imagin-grey&angle=01",
-    carLbs: "365",
-    carRwhp: "335",
-    carPrice: "49,990",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=dodge&modelFamily=challenger&modelRange=challenger-srt&modelVariant=co&modelYear=2021&powerTrain=fossil&transmission=0&bodySize=2&trim=us&paintId=pspc0096&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+    carLbs: "650", //pound-feet of torque
+    carRwhp: "475",
+    carPrice: "85,430",
     carType: "rwd",
-    url: "",
+    url: "https://www.edmunds.com/dodge/challenger/",
   },
   {
-    carName: "2022 Land Rover Range Rover Sport",
+    carName: "2022 Dodge Charger SRT Hellcat",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=land-rover&modelFamily=range-rover&modelRange=range-rover&modelVariant=od&modelYear=2022&powerTrain=fossil&transmission=0&bodySize=5&trim=eu&paintId=pspc0041&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=dodge&modelFamily=charger&modelRange=charger-srt&modelVariant=sa&modelYear=2021&powerTrain=petrol&transmission=0&bodySize=4&trim=us&paintId=pspc0352&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
     carLbs: "6,613",
-    carRwhp: "575",
-    carPrice: "49,990",
+    carRwhp: "717",
+    carPrice: "85,495",
     carType: "rwd",
     url: "",
   },
   {
     carName: "2019 Toyota Supra",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=lamborghini&modelFamily=aventador&modelRange=aventador-svj&modelVariant=co&modelYear=2019&powerTrain=petrol&transmission=0&bodySize=2&trim=eu&paintId=imagin-grey&angle=01",
-    carLbs: "365",
-    carRwhp: "335",
-    carPrice: "49,990",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=ford&modelFamily=mustang&modelRange=mustang-mach-e&modelVariant=od&modelYear=2020&powerTrain=fossil&transmission=0&bodySize=5&trim=0&paintId=pspc0146&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+    carLbs: "600",
+    carRwhp: "480",
+    carPrice: "61,995",
     carType: "rwd",
     url: "",
   },
@@ -121,7 +121,7 @@ const suvCarCollection: CollectionType[] = [
   {
     carName: "2022 Lamborghini Urus",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=lamborghini&modelFamily=urus&modelRange=urus&modelVariant=od&modelYear=2022&powerTrain=petrol&transmission=0&bodySize=5&trim=eu&paintId=pspc0041&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=lamborghini&modelFamily=urus&modelRange=urus&modelVariant=od&modelYear=2022&powerTrain=petrol&transmission=0&bodySize=5&trim=eu&paintId=pspc0115&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
     carLbs: "627",
     carRwhp: "641",
     carPrice: "245,381",
@@ -131,7 +131,7 @@ const suvCarCollection: CollectionType[] = [
   {
     carName: "2020 Mercedes-Benz G-Class",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=mercedes&modelFamily=g-class&modelRange=g-class&modelVariant=od&modelYear=2020&powerTrain=fossil&transmission=0&bodySize=5&trim=0&paintId=pspc0041&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=mercedes&modelFamily=g-class&modelRange=g-class&modelVariant=od&modelYear=2020&powerTrain=fossil&transmission=0&bodySize=5&trim=0&paintId=pspc0006&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
     carLbs: "6,500",
     carRwhp: "577",
     carPrice: "199,700",
@@ -141,17 +141,17 @@ const suvCarCollection: CollectionType[] = [
   {
     carName: "2022 Land Rover Range Rover Sport",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=land-rover&modelFamily=range-rover&modelRange=range-rover&modelVariant=od&modelYear=2022&powerTrain=fossil&transmission=0&bodySize=5&trim=eu&paintId=pspc0041&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=land-rover&modelFamily=range-rover&modelRange=range-rover&modelVariant=od&modelYear=2022&powerTrain=fossil&transmission=0&bodySize=5&trim=eu&paintId=pspc0074&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
     carLbs: "6,613",
     carRwhp: "575",
     carPrice: "49,990",
-    carType: "rwd",
-    url: "",
+    carType: "awd",
+    url: "https://www.edmunds.com/land-rover/range-rover-sport/",
   },
   {
     carName: "2022 Tesla Model X",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=tesla&modelFamily=model-x&modelRange=model-x&modelVariant=od&modelYear=2022&powerTrain=electric&transmission=0&bodySize=5&trim=eu&paintId=pspc0041&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=tesla&modelFamily=model-x&modelRange=model-x&modelVariant=od&modelYear=2022&powerTrain=electric&transmission=0&bodySize=5&trim=eu&paintId=pspc0115&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
     carLbs: "5,000",
     carRwhp: "670",
     carPrice: "127,990",
@@ -161,7 +161,7 @@ const suvCarCollection: CollectionType[] = [
   {
     carName: "2021 Audi RS Q8",
     carImage:
-      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=audi&modelFamily=q8&modelRange=q8&modelVariant=od&modelYear=2021&powerTrain=fossil&transmission=0&bodySize=5&trim=0&paintId=pspc0041&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
+      "https://cdn.imagin.studio/getImage?&customer=usrobert-s-website&make=audi&modelFamily=q8&modelRange=q8&modelVariant=od&modelYear=2021&powerTrain=fossil&transmission=0&bodySize=5&trim=0&paintId=pspc0115&fileType=png&zoomType=fullscreen&zoomLevel=100&width=1600&angle=01",
     carLbs: "7,700",
     carRwhp: "591",
     carPrice: "127,990",
