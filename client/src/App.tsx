12a440ad414f76d6292d5fd92483a176110e5169
Robert C. Haye <roberthaye1994@gmail.com>
2023-01-07 15:06:45 +0100

    added global styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import Home from './components/Home/Home';
 import Navigation from './components/Navigation/Navigation';
+import GlobalStyle from "./GlobalStyle";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <Navigation/>
       <Home/>
     </div>
---
+++ client/src/GlobalStyle.ts
@@ -0,0 +1,114 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  text-decoration: none;
+}
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+figure {
+  margin: 0;
+}
+
+h1 {
+  font-size: 60px;
+}
+h2 {
+  font-size: 40px;
+}
+p {
+  line-height: 1.5;
+}
+.container {
+  position: relative;
+  margin: 0 auto;
+  width: 80%;
+  height: 100%;
+  padding: 40px 0;
+}
+
+.row {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 15px 0;
+  min-height: auto;
+}
+
+.flex-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+  padding: 15px 0;
+
+  img {
+    margin-right: 15px;
+  }
+}
+
+.col {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 15px 0;
+
+  img {
+    margin-right: 15px;
+  }
+}
+
+.hidden {
+  display: none;
+  transition: opacity 1s ease-out;
+  opacity: 0;
+}
+
+.margin-top {
+  margin-top: 50px;
+}
+
+//Tablet view
+@media (max-width: $desktop) {
+  .container {
+    width: 90%;
+  }
+}
+
+//Tablet view
+@media only screen and (max-width: $laptop) {
+  h1 {
+    font-size: 45px;
+  }
+  h2 {
+    font-size: 35px;
+  }
+
+  .container {
+    width: 95%;
+  }
+  .row {
+    flex-wrap: wrap;
+    & > img {
+      margin-bottom: 15px;
+    }
+  }
+}
+
+//Phone View
+@media only screen and (max-width: $phone) {
+  h1 {
+    font-size: 38px;
+  }
+  h2 {
+    font-size: 28px;
+  }
+}
+
+ `;
+
+export default GlobalStyle;
