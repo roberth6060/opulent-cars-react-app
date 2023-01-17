c96753684e54d8708a234e72ae77794e79974ec4
Robert C. Haye <roberthaye1994@gmail.com>
2023-01-17 12:58:04 +0100

    updated app service

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Your backend server is up and running';
+    return 'Hello World! Your backend server is up and running Hello';
   }
 }
